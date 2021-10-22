15e0abb07f14fe01bb5e1080097b1e3a253ebb2e
tk0798 <kaantutuk1@hotmail.com>
2021-10-22 16:52:48 +0300

    oldumu

--- app.py
+++ app.py
@@ -6,14 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def home():
-    try:
 
-        if request.method == 'POST':
-            bot = WABot(request.json)
-            return bot.processing()
-        else:
-            return "Olmadı aşkım yemedii buda"
-    except Exception as e:
         return "sicti"
 if(__name__) == '__main__':
     app.run()
