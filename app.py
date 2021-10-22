37eb57181013e340e238fd22817e318bbd8b7573
tk0798 <kaantutuk1@hotmail.com>
2021-10-22 17:35:12 +0300

    oldumu

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, request
 from wabot import WABot
 import json
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
-def home():
-    return "sicti"
+
+from flask import Flask # flask kütüphanemizi projemize import ettik.
+
+app = Flask(__name__) # app değişkenizimizin Flask olduğunu belirttik.
+
+@app.route("/") # Endpoint imizi tanımladık.
+def hello(): # Bir fonksiyon oluşturduk.
+    return "Hello World" # Sitemizde görmek istediğimiz şeyi return ettik.
+
+if(__name__) == '__main__':
+    app.run()
